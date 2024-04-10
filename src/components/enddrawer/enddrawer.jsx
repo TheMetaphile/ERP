3be72ed4d4b5deffa7cec810a02a4f61c8386e7e
BhuvneshwarTyagi <bhanu68tyagi@email.com>
2024-04-10 12:43:41 +0530

    fee screen bug resolved

--- src/components/enddrawer/enddrawer.jsx
+++ src/components/enddrawer/enddrawer.jsx
@@ -4,7 +4,7 @@ import Notice from './notice.jsx';
 
 export default function Enddrawer() {
   return (
-    <div className="enddrawer overflow-auto h-screen w-80 mr-2 no-scrollbar">
+    <div className="enddrawer overflow-auto h-screen w-60 mr-2 no-scrollbar">
       <h6 className="flex justify-between text-sm mt-3">Daily Notice <span className="text-blue-500">See All</span></h6>
       <div className="dialyNotices w-full h-2/5 bg-blue-50 rounded-xl p-2 mt-3 mb-3 overflow-auto no-scrollbar">
         <Notice title="Events" description="Today All the student of class 1st to 8th come to Auditorium at 12:00."/>
--- src/components/fees/fees.jsx
+++ src/components/fees/fees.jsx
@@ -4,7 +4,7 @@ import TransactionRow from './utils/TransactionHistoryRow';
 
 export default function Fees() {
     return (
-        <div className="flex-1 mt-2 h-screen text-left ml-2 overflow-y-auto">
+        <div className="flex-1 mt-2 h-full w-full text-left ml-2 overflow-auto no-srollbar">
             <h1 className="mb-2 text-2xl">Fee Status</h1>
             <FeeStatusRow />
             <h1 className="mb-2 text-2xl">Fees Structure</h1>
