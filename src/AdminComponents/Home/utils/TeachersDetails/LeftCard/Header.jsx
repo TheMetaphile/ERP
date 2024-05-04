7e856ccfa8a4ed13c93150ede71003c6d8effd0d
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 <bhanu68tyagi@gmail.com>
2024-05-05 05:15:28 +0530

    all students screen done

--- src/AdminComponents/Home/utils/TeachersDetails/LeftCard/Header.jsx
+++ src/AdminComponents/Home/utils/TeachersDetails/LeftCard/Header.jsx
@@ -2,7 +2,7 @@ export default function Header(props){
     return (
         <div className="flex w-full justify-between py-2 px-4 bg-bg_blue h-fit rounded-t-lg border-b-2 border-black">
             {props.headings.map((heading) => (
-            <h1 className="w-32 text-lg font-medium">
+            <h1 className="w-28 text-lg font-medium">
                 {heading}
             </h1>
           ))}
