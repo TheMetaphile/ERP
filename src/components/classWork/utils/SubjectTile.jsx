8935259a47f85ad554c2cfd1445bf0ad587df95b
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 <bhanu68tyagi@gmail.com>
2024-05-08 01:28:58 +0530

    mobile responsive adjustment for student side

--- src/components/classWork/utils/SubjectTile.jsx
+++ src/components/classWork/utils/SubjectTile.jsx
@@ -4,7 +4,7 @@ export default function SubjectTile(props) {
     return (
         <Link to={`${props.subject}`} className=" flex shadow-md rounded-lg p-4 justify-between items-center self-center">
             <div className=" w-full flex flex-col tablet:flex-row justify-between items-center ">
-                <h1 className="font-medium text-xl mb-3">{props.subject}</h1>
+                <h1 className="font-medium tablet:text-xl mb-3">{props.subject}</h1>
                 <img src={props.image} alt="image" className="mobile:max-laptop:h-12 laptop:h-14 " />
             </div>
 
