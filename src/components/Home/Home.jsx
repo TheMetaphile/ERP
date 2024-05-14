50d06d934dbc253f05ba04cd493a34b02eec9370
BhuvneshwarTyagi <bhanu68tyagi@email.com>
2024-05-14 06:02:17 +0530

    adjustment

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -39,7 +39,7 @@ export default function Home() {
         <div className='flex-grow px-2 tablet:h-80 tablet:pb-3 mobile:max-tablet:mb-5'>
         <AllSubjectProgress />
         </div>
-        <div className='tablet:h-full pb-3 tablet:w-3/12 mobile:max-tablet:w-full  mobile:max-tablet:mb-4 px-2'>
+        <div className='tablet:h-full pb-3 mobile:max-tablet:w-full  mobile:max-tablet:mb-4 px-2'>
         <Doughnut chartData={data} title='Attendance Status'/>
         </div>
       </div>
