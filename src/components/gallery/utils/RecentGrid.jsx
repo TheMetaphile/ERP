cfe9b02294fcd3abdd7662750774312e569ae045
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 <bhanu68tyagi@gmail.com>
2024-05-17 06:16:03 +0530

    shailesh grid issue in gallery recent tab resolved

--- src/components/gallery/utils/RecentGrid.jsx
+++ src/components/gallery/utils/RecentGrid.jsx
@@ -3,26 +3,30 @@ import React from 'react'
 function RecentGrid() {
   return (
     <div className="grid grid-cols-4 gap-4">
-   
+
       <div className="col-span-1">
         <div className="border border-gray-400 p-4">First Column</div>
         <div className="border border-gray-400 p-4">Inner Column 1</div>
         <div className="border border-gray-400 p-4">Inner Column 2</div>
       </div>
 
-      
-      <div className="col-span-2">
-        <div className="grid grid-cols-3">
-          <div className="col-span-3 border border-gray-400 p-4">Bottom Row</div>
-          <div className="col-span-3  border border-gray-400 p-4">Combined Bottom Row</div>
-          <div className="border border-gray-400 p-4">Inner Column 3</div>
-          <div className="border border-gray-400 p-4">Inner Column 4</div>
-          <div className="border border-gray-400 p-4">Inner Column 5</div>
-        </div>
+
+      <div className="col-span-2 grid grid-rows-3 border border-gray-400">
+        
+          <div className="row-span-3 text-center self-center">
+            
+              Center container
+          </div>
+
+          <div className='grid grid-cols-3'>
+          <div className="border border-gray-400 p-4 col-span-1">Inner Column 3</div>
+          <div className="border border-gray-400 p-4 col-span-1">Inner Column 4</div>
+          <div className="border border-gray-400 p-4 col-span-1">Inner Column 5</div>
+          </div>
       </div>
 
 
-   
+
       <div className="col-span-1">
         <div className="border border-gray-400 p-4">Last Column</div>
         <div className="border border-gray-400 p-4">Inner Column 6</div>
