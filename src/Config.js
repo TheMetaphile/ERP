00f7fc66d6136206f93ecdaa38fd3adf56e2809a
Singh_Shailesh <shaileshsingh12396@gmail.com>
2024-07-09 11:10:27 +0530

    endpoints updated

--- src/Config.js
+++ src/Config.js
@@ -1,21 +1,21 @@
-const BASE_URL_Login = 'https://loginapi-y0aa.onrender.com';
-const BASE_URL_ClassTeacher = 'https://class-teacher.onrender.com';
-const BASE_URL_Exam = 'https://examapi-jep8.onrender.com';
-const BASE_URL_Fee = 'https://feeapi.onrender.com';
-const BASE_URL_Student_Leave = 'https://studentleaveapi.onrender.com';
-const BASE_URL_Attendence = 'https://attendance-api-lako.onrender.com';
-const BASE_URL_Subject = 'https://assignsubjectapi.onrender.com';
-const BASE_URL_TimeTableStructure = 'https://timetablestructureapi.onrender.com';
-const BASE_URL_TimeTable = 'https://timetableapi-1wfp.onrender.com';
-const BASE_URL_AskDoubt = 'https://askdoubts.onrender.com';
-const BASE_URL_Result = 'https://studentresult-ztxk.onrender.com';
-const BASE_URL_Homework = 'https://homework-gzik.onrender.com';
-const BASE_URL_TeacherLeave = 'https://teachersleave.onrender.com';
+const BASE_URL_Login = 'http://ec2-3-110-87-97.ap-south-1.compute.amazonaws.com:8007';
+const BASE_URL_ClassTeacher = 'http://ec2-3-110-87-97.ap-south-1.compute.amazonaws.com:8011';
+const BASE_URL_Exam = 'http://ec2-3-110-87-97.ap-south-1.compute.amazonaws.com:8014';
+const BASE_URL_Fee = 'http://ec2-3-110-87-97.ap-south-1.compute.amazonaws.com:8012';
+const BASE_URL_Student_Leave = 'http://ec2-3-110-87-97.ap-south-1.compute.amazonaws.com:8004';
+const BASE_URL_Attendence = 'http://ec2-3-110-87-97.ap-south-1.compute.amazonaws.com:8008';
+const BASE_URL_Subject = 'http://ec2-3-110-87-97.ap-south-1.compute.amazonaws.com:8013';
+const BASE_URL_TimeTableStructure = 'http://ec2-3-110-87-97.ap-south-1.compute.amazonaws.com:8010';
+const BASE_URL_TimeTable = 'http://ec2-3-110-87-97.ap-south-1.compute.amazonaws.com:8010';
+const BASE_URL_AskDoubt = 'http://ec2-3-110-87-97.ap-south-1.compute.amazonaws.com:8001';
+const BASE_URL_Result = 'http://ec2-3-110-87-97.ap-south-1.compute.amazonaws.com:8009';
+const BASE_URL_Homework = 'http://ec2-3-110-87-97.ap-south-1.compute.amazonaws.com:8002';
+const BASE_URL_TeacherLeave = 'http://ec2-3-110-87-97.ap-south-1.compute.amazonaws.com:8003';
 const BASE_URL_ClassWork = 'http://ec2-3-110-87-97.ap-south-1.compute.amazonaws.com:8000';
-const BASE_URL_Notice = 'https://notice-rn3j.onrender.com';
+const BASE_URL_Notice = 'http://ec2-3-110-87-97.ap-south-1.compute.amazonaws.com:8006';
 const BASE_URL_Git = 'https://update-model-for-prediction-api.onrender.com';
 const BASE_URL_FaceDetection = 'https://32e3-223-233-72-219.ngrok-free.app';
-const BASE_URL_TeacherAttendence ='https://teacherattendance.onrender.com';
+const BASE_URL_TeacherAttendence ='http://ec2-3-110-87-97.ap-south-1.compute.amazonaws.com:8005';
 // https://face-prediction.onrender.com/predict
 // https://32e3-223-233-72-219.ngrok-free.app
 export { BASE_URL_Login, BASE_URL_ClassTeacher, BASE_URL_Exam, BASE_URL_Fee, BASE_URL_Student_Leave, BASE_URL_Attendence, BASE_URL_Subject, BASE_URL_TimeTableStructure, BASE_URL_TimeTable, BASE_URL_AskDoubt, BASE_URL_Result, BASE_URL_Homework, BASE_URL_TeacherLeave, BASE_URL_ClassWork, BASE_URL_Notice, BASE_URL_Git, BASE_URL_FaceDetection, BASE_URL_TeacherAttendence};
