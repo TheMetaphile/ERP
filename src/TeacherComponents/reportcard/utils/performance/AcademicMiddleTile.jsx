732144ed11e0533ce98f809db305ac020c52d8d7
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 <bhanu68tyagi@gmail.com>
2024-07-12 16:52:36 +0530

    download functionality added in result

--- src/TeacherComponents/reportcard/utils/performance/AcademicMiddleTile.jsx
+++ src/TeacherComponents/reportcard/utils/performance/AcademicMiddleTile.jsx
@@ -1,45 +1,42 @@
 import AcademicTopTile from "./AcademicTopTile";
 import AcademicBottonTile from "./AcademicBottomTile";
+
 export default function AcademicMiddleTile({ details }) {
-    return (
-        <div className="border-b-2 mobile:max-tablet:w-fit w-full">
-            {details.term1.length > 0 && (
-                <div className="mt-2">
-
-                    <h2 className="font-medium text-lg px-3">Term 1</h2>
-                    <AcademicTopTile heading={["Subject", 'Obtained Practical Marks', 'Total Practical Marks', 'Obtained Marks', "Total Marks"]} />
-                    {details.term1.map((detail, index) => (
-                        <div key={index} className='mobile:max-tablet:w-fit w-full flex justify-between border border-gray-300 shadow-md items-center'>
-                            <div className='w-52 text-lg text-center py-2'>{detail.subject}</div>
-                            <div className='w-60 text-lg text-center py-2 bg-blue-200'>{detail.obtainedPracticalMarks}</div>
-                            <div className='w-60 text-lg text-center py-2 bg-green-200'>{detail.totalPracticalMarks}</div>
-                            <div className='w-60 text-lg text-center py-2 bg-blue-200'>{detail.marksObtained}</div>
-                            <div className='w-60 text-lg text-center py-2 bg-green-200'>{detail.totalMarks}</div>
-                        </div>
-                    ))}
-                    <AcademicBottonTile value={["", 'GPA', "8.2"]} />
-
-                </div>
-
-            )}
-
-            {details.term2.length > 0 && (
-                <div className="mt-3">
-                    <h2 className="font-medium text-lg px-3">Term 2</h2>
-                    <AcademicTopTile heading={["Subject", 'Obtained Practical Marks', 'Total Practical Marks', 'Obtained Marks', "Total Marks"]} />
-                    {details.term2.map((detail, index) => (
-                        <div key={index} className='mobile:max-tablet:w-fit w-full flex justify-between border border-gray-300 shadow-md items-center'>
-                            <div className='w-52 text-lg text-center py-2'>{detail.subject}</div>
-                            <div className='w-60 text-lg text-center py-2 bg-blue-200'>{detail.obtainedPracticalMarks}</div>
-                            <div className='w-60 text-lg text-center py-2 bg-green-200'>{detail.totalPracticalMarks}</div>
-                            <div className='w-60 text-lg text-center py-2 bg-blue-200'>{detail.marksObtained}</div>
-                            <div className='w-60 text-lg text-center py-2 bg-green-200'>{detail.totalMarks}</div>
-                        </div>
-                    ))}
-                    <AcademicBottonTile value={["", 'GPA', "8.2"]} />
-
-                </div>
-            )}
+  return (
+    <div className="border-b-2 mobile:max-tablet:w-fit w-full px-3">
+      {details.term1.length > 0 && (
+        <div className="term-section mt-2">
+          <h2 className="font-medium text-lg px-3 mb-3">Term 1</h2>
+          <AcademicTopTile heading={["Subject", 'Obtained Practical Marks', 'Total Practical Marks', 'Obtained Marks', "Total Marks"]} />
+          {details.term1.map((detail, index) => (
+            <div key={index} className='mobile:max-tablet:w-fit w-full flex justify-between border border-gray-300 shadow-md items-center'>
+              <div className='w-52 text-lg text-center py-2'>{detail.subject}</div>
+              <div className='w-60 text-lg text-center py-2 bg-blue-200'>{detail.obtainedPracticalMarks}</div>
+              <div className='w-60 text-lg text-center py-2 bg-green-200'>{detail.totalPracticalMarks}</div>
+              <div className='w-60 text-lg text-center py-2 bg-blue-200'>{detail.marksObtained}</div>
+              <div className='w-60 text-lg text-center py-2 bg-green-200'>{detail.totalMarks}</div>
+            </div>
+          ))}
+          <AcademicBottonTile value={["", 'GPA', "8.2"]} />
         </div>
-    );
+      )}
+
+      {details.term2.length > 0 && (
+        <div className="term-section mt-3">
+          <h2 className="font-medium text-lg px-3 mb-3">Term 2</h2>
+          <AcademicTopTile heading={["Subject", 'Obtained Practical Marks', 'Total Practical Marks', 'Obtained Marks', "Total Marks"]} />
+          {details.term2.map((detail, index) => (
+            <div key={index} className='mobile:max-tablet:w-fit w-full flex justify-between border border-gray-300 shadow-md items-center'>
+              <div className='w-52 text-lg text-center py-2'>{detail.subject}</div>
+              <div className='w-60 text-lg text-center py-2 bg-blue-200'>{detail.obtainedPracticalMarks}</div>
+              <div className='w-60 text-lg text-center py-2 bg-green-200'>{detail.totalPracticalMarks}</div>
+              <div className='w-60 text-lg text-center py-2 bg-blue-200'>{detail.marksObtained}</div>
+              <div className='w-60 text-lg text-center py-2 bg-green-200'>{detail.totalMarks}</div>
+            </div>
+          ))}
+          <AcademicBottonTile value={["", 'GPA', "8.2"]} />
+        </div>
+      )}
+    </div>
+  );
 }
--- src/TeacherComponents/reportcard/utils/performance/PerformanceProfile.jsx
+++ src/TeacherComponents/reportcard/utils/performance/PerformanceProfile.jsx
@@ -1,40 +1,83 @@
-import React, { useState, useEffect, useContext } from 'react'
+import React, { useState, useEffect, useRef, useContext } from 'react';
 import { useParams } from "react-router-dom";
 import InfoCard from "../../../../components/Result/utils/InfoCard";
 import Attendance from "./Attendence";
 import AcademicMiddleTile from "./AcademicMiddleTile";
-import jsPDF from "jspdf";
-import html2canvas from "html2canvas";
 import axios from "axios";
 import Loading from "../../../../LoadingScreen/Loading";
 import AuthContext from "../../../../Context/AuthContext";
 import { BASE_URL_Login, BASE_URL_Result } from "../../../../Config";
 import logo from '../../../../assets/school logo.png';
+import './Print.css';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
+const PrintableComponent = React.forwardRef((props, ref) => {
+  return (
+    <div ref={ref} className="your-component px-3">
+      <h3 className="text-xl font-medium">Performance Profile</h3>
+      <div className="w-full border border-gray-300 shadow-md rounded-lg p-4 mt-4">
+        <div className="flex justify-center">
+          <img src={logo} alt="img" className='mobile:max-tablet:w-20' />
+          <div className='self-center ml-3'>
+            <h1 className='tablet:text-3xl mobile:max-tablet:text-lg font-medium text-text_blue'>
+              Metaphile Public School
+            </h1>
+            <h3 className='tablet:text-xl text-gray-400 mb-4'>
+              'O' Block, Ganganagar, Meerut-250001
+            </h3>
+          </div>
+        </div>
+        <div className="border-t-2 border-text_blue my-2 tablet:mx-3 rounded-full"></div>
+        <h1 className='tablet:text-3xl mobile:max-tablet:text-xl font-medium text-text_blue text-center'>Performance Profile</h1>
+        <div className="border-t-2 border-text_blue my-3 tablet:mx-3 rounded-full"></div>
+        <div className='flex w-full justify-evenly mobile:max-tablet:flex-col'>
+          <div className='flex flex-col text-center items-center'>
+            <img src={props.profile.profileLink} alt="img" className=' w-20 h-20 rounded-full' crossOrigin="anonymous" />
+            <h1 className='mt-2 teblet:text-2xl mobile:max-tablet:text-xl font-medium '>{props.profile.name}</h1>
+            <h3 className='text-lg font-medium text-gray-400'>Class {props.profile.currentClass} {props.profile.section}</h3>
+          </div>
+          <div className='flex'>
+            <div className='text-lg font-medium tablet:w-60 mobile:max-tablet:w-48 my-2'>
+              <h1>Roll Number</h1>
+              <h1>Date of Birth</h1>
+              <h1>Blood Group</h1>
+              <h1>Contact No.</h1>
+              <h1>Class</h1>
+              <h1>Father's Name</h1>
+              <h1>Mother's Name</h1>
+            </div>
+            <div className='text-lg w-60 text-gray-400 my-2'>
+              <h1>{props.profile.rollNumber}</h1>
+              <h1>{props.profile.DOB}</h1>
+              <h1>{props.profile.bloodGroup}</h1>
+              <h1>{props.profile.fatherPhoneNumber}</h1>
+              <h1>{props.profile.currentClass} {props.profile.section}</h1>
+              <h1>{props.profile.fatherName}</h1>
+              <h1>{props.profile.motherName}</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Attendance term={[{ total: "249", attendance: "235" }]} />
+      <AcademicMiddleTile details={props.details} />
+      <div className="page-break"></div>
 
-export default function PerformanceProfile() {
+    </div>
+  );
+});
 
+const PerformanceProfile = () => {
   const { id } = useParams();
   const [loading, setLoading] = useState(true);
   const { authState } = useContext(AuthContext);
   const [details, setDetails] = useState([]);
   const [profile, setProfile] = useState([]);
   const [profileLoading, setProfileLoading] = useState(true);
-
-  const printDocument = () => {
-    const input = document.getElementById('divToPrint');
-    html2canvas(input).then((canvas) => {
-      const imgData = canvas.toDataURL('image/png');
-      const pdf = new jsPDF();
-      pdf.addImage(imgData, 'JPEG', 0, 0);
-      pdf.save('download.pdf');
-    });
-  };
-
+  const componentRef = useRef();
 
   useEffect(() => {
     const fetchProfile = async () => {
-      console.log({ id });
       setProfileLoading(true);
       try {
         const response = await axios.post(`${BASE_URL_Login}/fetchSingle/student`, {
@@ -43,139 +86,84 @@ export default function PerformanceProfile() {
         });
         if (response.status === 200) {
           fetchResult();
-          console.log('fetch single', response.data.StudentDetails[0]);
           setProfile(response.data.StudentDetails[0]);
-
         }
       } catch (error) {
-        console.error("Error fetching student single:", error);
+        console.error('Error fetching profile:', error);
+      }
+      setProfileLoading(false);
+    };
+
+    const fetchResult = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(`${BASE_URL_Result}/result/fetch/teacher?email=${id}`, {
+          headers: {
+            Authorization: `Bearer ${authState.accessToken}`,
+          }
+        });
+        if (response.status === 200) {
+          setDetails(response.data);
+        }
+      } catch (error) {
+        console.error("Error fetching student result:", error);
       } finally {
-        setProfileLoading(false);
+        setLoading(false);
       }
     };
+
     fetchProfile();
-  }, [authState.accessToken, id]);
+  }, [id, authState.accessToken]);
 
+  if (loading || profileLoading) {
+    return <Loading />;
+  }
 
+  const handlePrint = async () => {
+    const input = componentRef.current;
+  
+    const canvas = await html2canvas(input, {
+      useCORS: true, // Enable cross-origin images
+      logging: true,
+      allowTaint: false,
+    });
+    const imgData = canvas.toDataURL('image/png');
+    const pdf = new jsPDF('p', 'mm', 'a4');
+    const imgProps = pdf.getImageProperties(imgData);
+    const pdfWidth = pdf.internal.pageSize.getWidth();
+    const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+  
+    pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+    
+    // Save the PDF with a specified name
+    const pdfBlob = pdf.output('blob');
+    const pdfUrl = URL.createObjectURL(pdfBlob);
+    const iframe = document.createElement('iframe');
+    iframe.style.position = 'absolute';
+    iframe.style.width = '0';
+    iframe.style.height = '0';
+    iframe.src = pdfUrl;
+    document.body.appendChild(iframe);
+  
+    iframe.onload = () => {
+      iframe.contentWindow.print();
+    };
+  
 
-
-  const fetchResult = async () => {
-    console.log(profile.name)
-    console.log({ id });
-    setLoading(true);
-    try {
-      const response = await axios.get(`${BASE_URL_Result}/result/fetch/teacher?email=${id}`, {
-        headers: {
-          Authorization: `Bearer ${authState.accessToken}`,
-        }
-      });
-      if (response.status === 200) {
-        console.log(response.data);
-        setDetails(response.data);
-      }
-    } catch (error) {
-      console.error("Error fetching student result:", error);
-    } finally {
-      setLoading(false);
-    }
   };
+  
 
   return (
-    <div className="flex flex-col w-full h-screen overflow-y-auto items-start mt-2 px-2 no-scrollbar" id="divToPrint"  style={{
-      backgroundColor: '#f5f5f5',
-      width: '210mm',
-      minHeight: '297mm',
-      marginLeft: 'auto',
-      marginRight: 'auto'
-    }}>
-      {loading || profileLoading ? (
-        <Loading />
-      ) : details === null ? (
-        <>
-          <h3 className="text-xl font-medium">Performance Profile</h3>
-          <InfoCard
-            class={profile.currentClass}
-            name={profile.name}
-            profileImg={profile.profileLink}
-            section={profile.section}
-            rollnumber={profile.rollNumber}
-            dob={profile.DOB}
-            bloodgroup={profile.bloodGroup}
-            contactno={profile.fatherPhoneNumber}
-            father={profile.fatherName}
-            mother={profile.motherName}
-          />
-          <Attendance term={[{ total: "249", attendance: "235" }]} />
-          <div className='text-center text-lg text-red-500 font-medium w-full mt-2'>No Result available</div>
-        </>
-      ) : (
-        <>
-          <h3 className="text-xl font-medium">Performance Profile</h3>
-          {/* <InfoCard
-            class={profile.currentClass}
-            name={profile.name}
-            profileImg={profile.profileLink}
-            section={profile.section}
-            rollnumber={profile.rollNumber}
-            dob={profile.DOB}
-            bloodgroup={profile.bloodGroup}
-            contactno={profile.fatherPhoneNumber}
-            father={profile.fatherName}
-            mother={profile.motherName}
-          /> */}
-          <div className="w-full border border-gray-300 shadow-md rounded-lg p-4 mt-4 " >
-            <div className="flex justify-center">
-              <img src={logo} alt="img" className='mobile:max-tablet:w-20' />
-              <div className='self-center ml-3'>
-                <h1 className='tablet:text-3xl mobile:max-tablet:text-lg font-medium text-text_blue'>
-                  Metaphile Public School
-                </h1>
-                <h3 className='tablet:text-xl text-gray-400 mb-4'>
-                  'O' Block, Ganganagar, Meerut-250001
-                </h3>
-              </div>
-            </div>
-            <div className="border-t-2 border-text_blue my-2 tablet:mx-3 rounded-full "></div>
-            <h1 className='tablet:text-3xl mobile:max-tablet:text-xl font-medium text-text_blue text-center'>Performance Profile</h1>
-            <div className="border-t-2 border-text_blue my-3 tablet:mx-3 rounded-full "></div>
-            <div className='flex w-full justify-evenly mobile:max-tablet:flex-col' >
-              <div className='flex flex-col text-center items-center'>
-                <img src={profile.profileLink} alt="img" className=' w-20 h-20 rounded-full' />
-                <h1 className='mt-2 teblet:text-2xl mobile:max-tablet:text-xl font-medium '>{profile.name}</h1>
-                <h3 className='text-lg font-medium text-gray-400'>Class {profile.currentClass} {profile.section}</h3>
-              </div>
-              <div className='flex'>
-                <div className='text-lg font-medium tablet:w-60 mobile:max-tablet:w-48 my-2'>
-                  <h1>Roll Number</h1>
-                  <h1>Date of Birth</h1>
-                  <h1>Blood Group</h1>
-                  <h1>Contact No.</h1>
-                  <h1>Class</h1>
-                  <h1>Father's Name</h1>
-                  <h1>Mother's Name</h1>
-                </div>
-                <div className='text-lg w-60 text-gray-400 my-2'>
-                  <h1>{profile.rollNumber}</h1>
-                  <h1>{profile.DOB}</h1>
-                  <h1>{profile.bloodGroup}</h1>
-                  <h1>{profile.fatherPhoneNumber}</h1>
-                  <h1>{profile.currentClass} {profile.section}</h1>
-                  <h1>{profile.fatherName}</h1>
-                  <h1>{profile.motherName}</h1>
-                </div>
-              </div>
-            </div>
-          </div>
-          <Attendance term={[{ total: "249", attendance: "235" }]} />
-          {/* <Academic /> */}
-          <AcademicMiddleTile details={details} />
-          <div className="text-xl font-medium my-3 bg-secondary text-black self-center rounded-lg shadow-md py-1 px-3 mt-3 hover:bg-blue-400 cursor-pointer hover:text-white" onClick={printDocument}>
-            Download
-          </div>
-        </>
-      )
-
-      }
+    <div className="mt-4 w-full">
+      <button
+        className="text-xl font-medium bg-secondary text-black rounded-lg shadow-md py-1 px-3 hover:bg-blue-400 cursor-pointer hover:text-white"
+        onClick={handlePrint}
+      >
+        Download
+      </button>
+      <PrintableComponent ref={componentRef} profile={profile} details={details} />
     </div>
   );
-}
+};
+
+export default PerformanceProfile;
