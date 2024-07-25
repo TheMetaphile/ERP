09e955cc24dd71a10b9ba51ce5da34c1ab0cbcdb
Singh_Shailesh <shaileshsingh12396@gmail.com>
2024-07-25 10:37:29 +0530

    classwork adjustment

--- src/TeacherComponents/classwork/ClassWorkTile.jsx
+++ src/TeacherComponents/classwork/ClassWorkTile.jsx
@@ -96,19 +96,20 @@ export default function ClassWorkTile({ details, Class, additionalData, selected
         <div className="mb-4 mt-3 items-center justify-between">
             {editedDetails.map((detail, index) => (
                 <div key={index} className='w-full  flex-col border border-gray-200 p-2 rounded-lg shadow-md mt-3'>
-                    <div className="flex items-center justify-between cursor-pointer" onClick={() => handleClick(index)}>
+                    <div className="flex mobile:max-tablet:flex-col items-center justify-between cursor-pointer" onClick={() => handleClick(index)}>
                         {editingRow === index ? (
                             <>
-                                <div className="pl-2 font-medium">Chapter: </div>
+                                <div className='flex gap-2 items-center w-full'>
+                                    <div className="pl-2 font-medium">Chapter: </div>
 
-                                <input
-                                    className="font-normal border border-gray-300 shadow-md rounded-lg px-2 py-1 text-justify"
-                                    value={detail.chapter}
-                                    onChange={(e) => handleInputChange(index, 'chapter', e.target.value)}
-                                />
-
-                                <div className='flex gap-2 justify-end'>
-                                    {/* <button
+                                    <input
+                                        className="font-normal border border-gray-300 shadow-md rounded-lg px-2 py-1 text-justify"
+                                        value={detail.chapter}
+                                        onChange={(e) => handleInputChange(index, 'chapter', e.target.value)}
+                                    />
+                                </div>
+                                {/* <div className='flex gap-2 justify-end'> */}
+                                {/* <button
                                         className='bg-green-400 hover:bg-green-700 text-white px-3 py-1 rounded-lg shadow-md flex items-center'
                                         onClick={() => handleConfirmClick(index)}
                                     >
@@ -120,20 +121,23 @@ export default function ClassWorkTile({ details, Class, additionalData, selected
                                     >
                                         <FaTimes />
                                     </button> */}
+                                <div className='flex gap-3 items-center w-full mt-2'>
+                                    <div className="pl-2 font-medium">Subject: </div>
                                     <input
                                         className="font-normal  border border-gray-300 shadow-md rounded-lg px-2 py-1 text-justify"
                                         value={detail.subject}
                                         onChange={(e) => handleInputChange(index, 'subject', e.target.value)}
                                     />
                                 </div>
+                                {/* </div> */}
 
 
                             </>
                         ) : (
                             <>
-
-                                <div className="pl-2 font-medium">Chapter: <span className='font-normal'>{detail.chapter}</span></div>
-                                <div className='flex items-center gap-1'>
+                                <div className='flex gap-2 items-center justify-between w-full'>
+                                    <div className="pl-2 font-medium">Chapter: <span className='font-normal'>{detail.chapter}</span></div>
+                                    {/* <div className='flex items-center gap-1'> */}
 
                                     {/* <button
                                         className='bg-blue-400 hover:bg-blue-700 text-white px-3 py-1 rounded-lg shadow-md flex items-center'
@@ -153,9 +157,9 @@ export default function ClassWorkTile({ details, Class, additionalData, selected
                                         {detail.subject}
 
                                     </div>
+                                    {/* </div> */}
+
                                 </div>
-
-
 
                             </>
                         )}
